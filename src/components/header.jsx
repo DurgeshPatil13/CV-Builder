0bc1d8090c46b33647fab98f432a56032b64843d
DurgeshPatil13 <durgeshpatil0513@gmail.com>
2026-06-28 22:15:12 +0530

    Initial commit

---
+++ src/components/header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './header.css'
+function Header  ()  {
+  return (
+    <div className='header-container'>
+
+<h1 className='header-heading'><span className='heading2'>CV</span>Builder</h1>
+<span className='othertext'>Fill in your Details → </span>
+    </div>
+  )
+}
+
+export default Header
